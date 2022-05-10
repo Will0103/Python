5163273c890b9d5a33ecd61d455001a88bb7f43e
Will0103 <57029196+Will0103@users.noreply.github.com>
2022-05-10 22:00:07 +0800

    Update Prime number.py

--- Prime number.py
+++ Prime number.py
@@ -1,8 +1,9 @@
 max_value=eval(input("輸入最大值 : "))
 print("質數有 : ",end="")
 for i in range(2,max_value+1):
-    j = i - 1
-    while i % j != 0 :
-        j = j - 1
-    if j == 1:
-        print(i,", ",end="")    
+    for j in range(2,i):
+        if i % j == 0:
+           break
+    else:
+        print(i,end=" ")    
+        
