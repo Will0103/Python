544cbb1bf61490d5bbba049f8fd0f44b2ff783e5
Will0103 <57029196+Will0103@users.noreply.github.com>
2022-05-16 20:46:31 +0800

    Create lotto.py

---
+++ lotto.py
@@ -0,0 +1,12 @@
+def choice():
+    return random.choice(range(1,50))
+
+import random
+A=[]
+while len(A) <6 :
+    a=choice()
+    if a not in A :
+        A.append(a)
+A.sort()
+print(A)
+        
